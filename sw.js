1f5f288b43be1e09458861731e8db5300ae39a99
plarika <148452619+plarika@users.noreply.github.com>
2026-08-19 07:53:38 +0100

    Maintenance: refresh ReVanced issue alerts

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'revanced-manual-v3.1.0-public-ui-r13';
+const CACHE_VERSION = 'revanced-manual-v3.1.0-public-ui-r14';
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
@@ -7,7 +7,7 @@ const CORE_ASSETS = [
   './index.html',
   './assets/style.css?v=3.1.0-r12',
   './assets/app.js?v=3.1.0-r12',
-  './assets/manual-data.js?v=3.1.0-r13',
+  './assets/manual-data.js?v=3.1.0-r14',
   './manifest.webmanifest?v=3.1.0-r12',
   './icons/icon-192.png',
   './icons/icon-512.png',
